de96894b1640877eb1f4807e7c734329e4fd4853
Cristian <cg082490@gmail.com>
2022-07-07 06:52:47 -0500

    Se crearon modulos sidebar

--- src/app/pais/pages/por-capital/por-capital.component.ts
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-por-capital',
@@ -6,11 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class PorCapitalComponent implements OnInit {
+export class PorCapitalComponent{
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
 
 }
